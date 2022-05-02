7303f23cabef9c408823e6a63df37588803c4972
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-03 02:04:34 +0530

    Add project files.

---
+++ Upper case char find/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper_case_char_find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value :");
+            char data = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("This is value {0} ",data);
+
+            int f =  32+data;
+
+            char result = (char)f;
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
